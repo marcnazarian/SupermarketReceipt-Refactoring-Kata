7b38fe343356644eb6b2fafa22f6f5669d0a641f
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-12-16 23:51:00 +0100

    move offerlogic to SpecialOfferType

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -39,7 +39,9 @@ public class ShoppingCart {
             if (offers.containsKey(p)) {
                 Offer offer = offers.get(p);
                 double unitPrice = catalog.getUnitPrice(p);
-                Discount discount = calculateDiscount(p, quantity, offer, unitPrice);
+
+                SpecialOfferType offerType = offer.offerType;
+                Discount discount = offerType.calculateDiscount(p, quantity, unitPrice, offer.argument);
 
                 if (discount != null) {
                     receipt.addDiscount(discount);
@@ -49,28 +51,4 @@ public class ShoppingCart {
         }
     }
 
-    private Discount calculateDiscount(Product p, double quantity, Offer offer, double unitPrice) {
-        Discount discount;
-        if (offer.offerType == SpecialOfferType.TwoForAmount && quantity >= 2) {
-            double total = offer.argument * quantity / 2 + quantity % 2 * unitPrice;
-            double discountN = unitPrice * quantity - total;
-            discount = new Discount(p, "2 for " + offer.argument, discountN);
-
-        } else if (offer.offerType == SpecialOfferType.ThreeForTwo && quantity > 2) {
-            int numberOfXs =  (int) quantity / 3;
-            double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantity % 3 * unitPrice);
-            discount = new Discount(p, "3 for 2", discountAmount);
-
-        } else if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-            discount = new Discount(p, offer.argument + "% off", quantity * unitPrice * offer.argument / 100.0);
-
-        } else if (offer.offerType == SpecialOfferType.FiveForAmount && quantity >= 5) {
-            int numberOfXs = (int) quantity / 5;
-            double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantity % 5 * unitPrice);
-            discount = new Discount(p, 5 + " for " + offer.argument, discountTotal);
-        } else {
-            discount = null;
-        }
-        return discount;
-    }
 }
